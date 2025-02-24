ab343fa706dd64eae2024e41fd3519c842aa338f
Pedro Lucas <pedrolucaspro@outlook.com>
2025-02-24 10:54:58 +0000

    commited LAB_12

---
+++ InputOutput.py
@@ -0,0 +1,16 @@
+'''
+   Programador...: (C) 2025 Pedro Sousa
+   Data..........: 12/02/2025
+   Observações...: Simple ChatBot em Python
+'''
+
+print("Seja Bem-vindo ao Torga Bot")
+
+nome = input("Qual é o seu nome? ")
+idade = int(input("Quantos anos tem? "))
+cidade = input("Vive em que cidade? ")
+interesse = input("O que gosta de fazer? ")
+
+resultado = f"Olá {nome}, tem {idade} ano(s) de idade, vive em {cidade} e gosta de {interesse}."
+
+print(resultado)
---
+++ README.md
@@ -0,0 +1,2 @@
+# LAB_08
+## Algoritimo que demonstra o funcionamento de um INPUT e OUTPUT.
